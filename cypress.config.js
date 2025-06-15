68028701caf0c2586a1477606eb53e15002a37a8
Diego <droxo63@gmail.com>
2025-06-15 15:22:46 -0300

    feat: adiciona workflow GitHub Actions com Cypress Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "9x52vw",
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementar hooks ou listeners se necessário
     },
-    baseUrl: "https://front.serverest.dev/"
+    baseUrl: "https://front.serverest.dev/",
   },
 });
